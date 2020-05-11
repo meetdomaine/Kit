1351d464ecbdcd2a1326525a02d51589076902a9
Max Rolon <max@halfhelix.com>
2020-05-11 12:17:00 -0500

    Attempt to resolve CI issue by pushing cypress version down

--- packages/cypress-kit/src/cypress/plugins.js
+++ packages/cypress-kit/src/cypress/plugins.js
@@ -1,7 +1,7 @@
 module.exports = (on, config) => {
   on('before:browser:launch', (browser, launchOptions) => {
     if (browser.name === 'chrome') {
-      launchOptions.args.push('--window-size=1600,1050')
+      launchOptions.push('--window-size=1600,1050')
       return launchOptions
     }
   })
