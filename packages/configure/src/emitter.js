bee15e63362729cb7a1abd77f4e20e12ca10604e
Max Rolon <max@halfhelix.com>
2022-08-24 19:38:47 -0400

    Fixes emitter issue with 1.1.2

--- packages/configure/src/emitter.js
+++ packages/configure/src/emitter.js
@@ -2,7 +2,7 @@ const utils = require('./utils')
 
 module.exports = {
   $emit(event, payload) {
-    if (this.$listeners[event]) {
+    if (this.$listeners && this.$listeners[event]) {
       this.$listeners[event](payload, utils, this)
     }
   }
